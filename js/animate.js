6a81034866e8bcabe33bcdf70b2cdde5a33f3707
Jared Gonzales <jrg2221@columbia.edu>
2020-10-28 03:02:40 -0600

    new anim

--- js/animate.js
+++ js/animate.js
@@ -17,7 +17,7 @@ function playInfitapLoad(rive) {
         const infitapAnimInstance = new rive.LinearAnimationInstance(infitapAnim);
 
         // Render animation
-        const canvas = document.getElementById('infitapAnimation');
+        const canvas = document.getElementById('yctAnimation');
         const ctx = canvas.getContext('2d');
         const renderer = new rive.CanvasRenderer(ctx);
         artboard.advance(0);
